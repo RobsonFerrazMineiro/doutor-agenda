087fc4f405c168d426edcb9b103c12c17f6001c2
Robson Ferraz Mineiro <robson_r_f@hotmail.com>
2025-06-08 23:31:24 -0300

    feat: ensure doctor availability is saved as UTC on database

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -4,8 +4,12 @@ import { db } from "@/db";
 import { doctorsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { actionClient } from "@/lib/next-safe-action";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { headers } from "next/headers";
 import { upsertDoctorSchema } from "./schema";
+
+dayjs.extend(utc);
 
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
@@ -20,17 +24,36 @@ export const upsertDoctor = actionClient
       throw new Error("Clinic not found ");
     }
 
+    const availableFromTime = parsedInput.availableFromTime;
+    const availableToTime = parsedInput.availableToTime;
+
+    const availableFromTimeUTC = dayjs()
+      .set("hour", parseInt(availableFromTime.split(":")[0]))
+      .set("minute", parseInt(availableFromTime.split(":")[1]))
+      .set("second", parseInt(availableToTime.split(":")[2]))
+      .utc();
+
+    const availableToTimeUTC = dayjs()
+      .set("hour", parseInt(availableToTime.split(":")[0]))
+      .set("minute", parseInt(availableToTime.split(":")[1]))
+      .set("second", parseInt(availableToTime.split(":")[2]))
+      .utc();
+
     await db
       .insert(doctorsTable)
       .values({
+        ...parsedInput,
         id: parsedInput.id,
         clinicId: session?.user.clinic?.id,
-        ...parsedInput,
+        availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+        availableToTime: availableToTimeUTC.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
         set: {
           ...parsedInput,
+          availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+          availableToTime: availableToTimeUTC.format("HH:mm:ss"),
         },
       });
   });
