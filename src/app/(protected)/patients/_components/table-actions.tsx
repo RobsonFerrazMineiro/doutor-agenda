70e6524942c8c9827f603f53c2d94f59a81f6933
Robson Ferraz Mineiro <robson_r_f@hotmail.com>
2025-06-18 22:25:41 -0300

    feat: ensure patient can be updated

---
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import { useState } from "react";
+import { UpsertPatientForm } from "./upsert-patient-form";
+
+interface PatientTableActionsProps {
+  patient: typeof patientsTable.$inferSelect;
+}
+
+const PatientTableActions = ({ patient }: PatientTableActionsProps) => {
+  const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+
+  return (
+    <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant={"ghost"} size="icon">
+            <MoreVerticalIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
+            <EditIcon /> Editar
+          </DropdownMenuItem>
+          <DropdownMenuItem variant="destructive">
+            <TrashIcon /> Excluir
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <UpsertPatientForm
+        isOpen={upsertDialogIsOpen}
+        patient={patient}
+        onSuccess={() => setUpsertDialogIsOpen(false)}
+      />
+    </Dialog>
+  );
+};
+
+export default PatientTableActions;
--- src/app/(protected)/patients/_components/table-columns.tsx
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
 import { ColumnDef } from "@tanstack/react-table";
 
-import { Button } from "@/components/ui/button";
 import { patientsTable } from "@/db/schema";
-import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import PatientTableActions from "./table-actions";
 
 type Patient = typeof patientsTable.$inferSelect;
 
@@ -46,25 +36,7 @@ export const patientTableColumns: ColumnDef<Patient>[] = [
     id: "actions",
     cell: (params) => {
       const patient = params.row.original;
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Button variant={"ghost"} size="icon">
-              <MoreVerticalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <EditIcon /> Editar
-            </DropdownMenuItem>
-            <DropdownMenuItem variant="destructive">
-              <TrashIcon /> Excluir
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+      return <PatientTableActions patient={patient} />;
     },
   },
 ];
